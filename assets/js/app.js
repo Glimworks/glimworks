bfd3032a8992eb8cf56319aeecd56125bd2df355
Beff <Beff.Henriksson@live.se>
2021-09-28 09:38:55 +0200

    working code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,14 @@
-import openOffcanvas from "./offcanvas.js";
-import closeOffcanvas from "./offcanvas.js";
+// Import
+import { openOffcanvas, closeOffcanvas } from "./offcanvas.js";
+
+// Target DOM-Elements
+const btnOpenOffcanvas = document.getElementById("openOffcanvas");
+const btnCloseOffcanvas = document.getElementById("closeOffcanvas");
+
+// EventListener
+btnOpenOffcanvas.addEventListener("click", () => {
+    openOffcanvas();
+});
+btnCloseOffcanvas.addEventListener("click", () => {
+    closeOffcanvas();
+});
